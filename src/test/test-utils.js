f839d6c6d3dba6efe3a7ef87e9fa3b0f51f50ba8
Sivaraj M <sivaraj2siva@gmail.com>
2026-05-11 13:41:35 +0530

    removed final solutions from repo

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import {render as rtlRender} from '@testing-library/react'
-import {ThemeProvider} from 'components/theme'
+import {render as rtlRender, screen} from '@testing-library/react'
+import {ThemeProvider} from '../components/theme'
 
 function render(ui, {theme = 'light', ...options} = {}) {
   const Wrapper = ({children}) => (
@@ -11,4 +11,4 @@ function render(ui, {theme = 'light', ...options} = {}) {
 
 export * from '@testing-library/react'
 // override React Testing Library's render with our own
-export {render}
+export {render, screen}
