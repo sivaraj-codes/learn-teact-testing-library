d4c4744d5055f35e8662152568d0e282dd6936bb
Sivaraj M <sivaraj2siva@gmail.com>
2026-05-13 10:50:39 +0530

    mockData Generation using test-data-bot

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -1,12 +1,43 @@
 // form testing
 // http://localhost:3000/login
 
-import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import {jest} from '@jest/globals'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-test('submitting the form calls onSubmit with username and password', () => {
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+
+  // screen.getByLabelText(/username/i)
+
+  // let username = 'userq'
+  // let password = 'password no'
+
+  const {username, password} = buildLoginForm()
+  console.log('Data', {username, password})
+  await userEvent.type(
+    screen.getByRole('textbox', {name: /username/i}),
+    username,
+  )
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  // screen.debug()
+  // expect(submittedData).toEqual({username, password})
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+
+  // console.log('ll', screen.getByRole('textbox', {name: /username/i}).nodeValue)
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
